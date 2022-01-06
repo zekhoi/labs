fbf0cae46312ee19e1d20f7e52235bc561835f25
Khoironi Kurnia Syah <55708473+zekhoi@users.noreply.github.com>
2022-01-06 13:19:29 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        base: "#f5f5f0",
+      },
+    },
+  },
+  plugins: [],
+};
